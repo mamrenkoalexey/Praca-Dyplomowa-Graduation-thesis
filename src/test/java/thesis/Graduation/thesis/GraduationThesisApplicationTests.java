8c558a6cb68378e97490c6d853fda71b7335eaa7
Lexus <mamrenko.alexey@gmail.com>
2025-08-11 13:34:30 +0200

    first commit, create project.

---
+++ src/test/java/thesis/Graduation/thesis/GraduationThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package thesis.Graduation.thesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
